b7347c2e8cddf35ab37d04f85e939e5cfee1ada7
kotan4ik <kostyuk.forgit@gmail.com>
2025-01-30 22:23:03 +0800

    feat: UserData model added

---
+++ src/main/java/com/kostyuk/models/UserData.java
@@ -0,0 +1,17 @@
+package com.kostyuk.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserData {
+    private String id;
+    private String email;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    private String avatar;
+}
