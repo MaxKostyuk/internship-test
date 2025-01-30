1cfea5b8f762a30db88fba685b45be92db251146
kotan4ik <kostyuk.forgit@gmail.com>
2025-01-30 21:09:14 +0800

    feat: checkErrorResponse method added to BaseRequest

--- src/main/java/com/kostyuk/requests/BaseRequest.java
+++ src/main/java/com/kostyuk/requests/BaseRequest.java
@@ -1,12 +1,14 @@
 package com.kostyuk.requests;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kostyuk.models.ErrorResponse;
 import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class BaseRequest {
@@ -17,6 +19,10 @@ public class BaseRequest {
         RestAssured.baseURI = "https://reqres.in/api";
         mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        RestAssured.config = RestAssured.config()
+                .objectMapperConfig(io.restassured.config.ObjectMapperConfig.objectMapperConfig()
+                        .jackson2ObjectMapperFactory((cls, charset) -> mapper));
     }
 
     protected static <T> T fromJson(String jsonString, Class<T> classOfT) throws Exception {
@@ -26,5 +32,13 @@ public class BaseRequest {
     @Step("Checking response code is equal to expected {expCode}")
     public static void checkResponseCode(Response response, int expCode) {
         assertEquals(expCode, response.statusCode(), "Received response code " + response.statusCode() + " is not equal to expected " + expCode);
+    }
+
+    @Step("Checking error response is correct and contains message {message}")
+    public static void checkErrorResponse(Response response, String message) {
+        ErrorResponse responseObject = assertDoesNotThrow(() -> fromJson(response.asString(), ErrorResponse.class), "Response has unexpected structure");
+        assertNotNull(responseObject.getError(), "Error message shouldn't be null");
+        assertEquals(message, responseObject.getError(), "Received error message " + responseObject.getError() + " is not equal to expected " + message);
+
     }
 }
