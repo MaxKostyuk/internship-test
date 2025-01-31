1407c918103ca292802deda5bb63a939f434d1a8
kotan4ik <kostyuk.forgit@gmail.com>
2025-01-31 15:15:54 +0800

    feat: in TestUtils generateCorrectUserUpdateData method added

--- src/main/java/com/kostyuk/utils/TestUtils.java
+++ src/main/java/com/kostyuk/utils/TestUtils.java
@@ -8,4 +8,8 @@ public class TestUtils {
         //В связи с ограниченностью функционала Reqres здесь задаются конкретные значения, а не генерируются случайные
         return Map.of("email", "eve.holt@reqres.in", "password", "pistol");
     }
+
+    public static Map<String, String> generateCorrectUserUpdateData() {
+        return Map.of("name", "morpheus", "job", "zion resident");
+    }
 }
