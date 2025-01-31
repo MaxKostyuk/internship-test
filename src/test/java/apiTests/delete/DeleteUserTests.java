3a7c67d78620c576d648238f955e54a500253e8e
kotan4ik <kostyuk.forgit@gmail.com>
2025-01-31 12:34:20 +0800

    feat: DeleteUserTests created with positive case

---
+++ src/test/java/apiTests/delete/DeleteUserTests.java
@@ -0,0 +1,28 @@
+package apiTests.delete;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.kostyuk.requests.BaseRequest.checkResponseCode;
+import static com.kostyuk.requests.BaseRequest.checkResponseHasNoBody;
+import static com.kostyuk.requests.User.deleteUser;
+
+@Epic("Api tests")
+@Feature("Delete user request tests")
+@DisplayName("Delete user request tests")
+public class DeleteUserTests {
+
+    @Test
+    @DisplayName("Positive test")
+    @Description("Should return code 204 and no body")
+    public void positiveShouldReturnCode204AndNoBody() {
+        Response response = deleteUser(2);
+        checkResponseCode(response, HttpStatus.SC_NO_CONTENT);
+        checkResponseHasNoBody(response);
+    }
+}
